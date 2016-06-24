a19c4e28a62cb518d71e845f761641d5c357551c
NickProdromou <hello@nickpro.com.au>
2016-06-24 15:40:15 +1000

    Trying to implement conditional logic in conversation flow

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ process.env.NOW_URL
  *
  */
 var Botkit = require('botkit');
+
+
+var Os = {
+  windows : "windows",
+  mac : "mac"
+}
+
+
+
 var controller = Botkit.slackbot();
 var bot = controller.spawn({
   token: process.env.SLACKBOT_TOKEN
@@ -24,6 +33,34 @@ bot.startRTM(function(error, whichBot, payload) {
   }
 });
 
-controller.hears(['Hello'], ['mention'], function(whichBot, message) {
-  whichBot.reply(message, 'Hello Nick!');
+
+controller.hears(['question me'],['direct_message','direct_mention','mention','ambient'],function(bot,message) {
+
+  // start a conversation to handle this response.
+  bot.startConversation(message,function(err,convo) {
+
+    convo.ask('Windows or mac?',function(response,convo) {
+  // say a different thing based on user response
+      if (response.text = Os.windows ) {
+        convo.say("okay, windows user eh?")
+      } else if (response.text = Os.mac ) {
+        convo.say("alright, we have a mac user")
+      } else {
+        convo.say("sorry" + response.text + "isn't known to me yet")
+      }
+
+    });
+
+  })
+
+});
+
+
+
+
+controller.hears(['hello'], ['mention'], function(whichBot, message) {
+  whichBot.reply(message, 'Hello, I am hotkey bot, I can tell you shortcuts for your programs \n'
+  + "Just tell me if you are using if you are using *OSX* or *WINDOWS* and we can get started" + Os.mac);
+
+
 });
